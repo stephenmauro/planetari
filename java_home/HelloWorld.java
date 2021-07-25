d1d665929ab382ff7ea770fe4bee41f825a0f2f8
Stephen M <stephenmauro@yahoo.com>
2021-07-25 13:10:37 -0400

    Added Java Launch

--- java_home/HelloWorld.java
+++ java_home/HelloWorld.java
@@ -3,5 +3,10 @@ package java_home;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World Again");
+        for (String arg : args) {
+            System.out.print(arg);
+            System.out.print(" ");
+        }
+        System.out.println();
     }
 }
